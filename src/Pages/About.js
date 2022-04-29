413b55375e4d47f6398be7d978d21ff4285204f1
ado durak <76266634+Alginest@users.noreply.github.com>
2022-04-29 21:47:48 +0200

    Add files via upload

---
+++ src/Pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutHeader from "../aboutComponents/AboutHeader/AboutHeader";
+import BitCoinInfo from "../aboutComponents/BitCoinInfo/BitCoinInfo";
+
+const About = () => {
+  return (
+    <>
+      <AboutHeader />
+      <BitCoinInfo />
+    </>
+  );
+};
+
+export default About;
